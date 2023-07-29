46a70cd0bfa522c3a35cb2e3e89d4ade777ebdb2
keith <keith@calvelli.dev>
2023-07-29 09:50:40 -0400

    Allowed insecure webkit

--- modules/office/default.nix
+++ modules/office/default.nix
@@ -1,6 +1,11 @@
  { confiig, pkgs, ... }:
 
  {
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "qtwebkit-5.212.0-alpha4"
+   ];
+
   environment.systemPackages = with pkgs; [
     calligra 
   ]; 
