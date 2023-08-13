ac052d8e099eaff7952a26f5ae3b91c78a490f07
Keith Calvelli <keith@calvelli.dev>
2023-08-13 17:27:53 -0400

    Added hp folder

---
+++ modules/hp/default.nix
@@ -0,0 +1,33 @@
+ 
+{ inputs, config, pkgs, ... }:
+{
+  hardware = {
+    bluetooth.enable = true;
+    pulseaudio.enable = false;
+    opengl = {
+      extraPackages = with pkgs; [
+        rocm-opencl-icd
+        rocm-opencl-runtime
+        amdvlk
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+      driSupport = true;
+      #driSupport32Bit = true;
+    };
+  };
+
+  boot = {
+    # Kernel
+    kernelParams = [
+      "quiet" "iommu=pt"   
+    ];
+    kernelModules = [ "amdgpu" ];
+
+    initrd = {
+    initrd = {
+      systemd.enable = true;
+      kernelModules = [ "amdgpu"];
+    };
+  };
+}
