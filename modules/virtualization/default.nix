2e4d7a11aea0764a439dd3af0bfc3a70cf97589f
keith <keith@calvelli.dev>
2023-08-19 10:59:33 -0400

    virtualization changes

--- modules/virtualization/default.nix
+++ modules/virtualization/default.nix
@@ -1,8 +1,8 @@
 { inputs, config, pkgs, ... }:
 {
+  users.extraUsers.keith.extraGroups = [ "libvirtd" ];
   virtualisation = {
     libvirtd.enable = true;
-
     podman = {
       enable = true;
 
@@ -18,4 +18,10 @@
     qemu
     distrobox
   ];
+
+  boot.extraModprobeConfig = ''
+    options kvm_amd nested=1
+    options kvm_amd emulate_invalid_guest_state=0
+    options kvm ignore_msrs=1
+  '';
 } 
