bd1c99f484fc560e96ae6e7e2ffeaa08a791a168
keith <keith@calvelli.dev>
2023-08-19 15:37:46 -0400

    Removed bridge

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -13,22 +13,33 @@
     # Use iwd instead of wpa_supplicant
     wireless.iwd.enable = true;
     networkmanager.wifi.backend = "iwd";
-
-    # Bridge network
-    interfaces.enp39s0.useDHCP = true;
-    interfaces.br0.useDHCP = true;
-    bridges = {
-      "br0" = {
-        interfaces = [ "enp39s0" ];
-      };
-    };
   };
 
   services = {
     avahi = {
       enable = true;
       nssmdns = true;
-      openFirewall = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        domain = true;
+        hinfo = true;
+        userServices = true;
+        workstation = true;
+      };
+    extraServiceFiles = {
+      smb = ''
+        <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+        <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+        <service-group>
+          <name replace-wildcards="yes">%h</name>
+          <service>
+            <type>_smb._tcp</type>
+            <port>445</port>
+          </service>
+        </service-group>
+      '';
+      };
     };
     openssh.enable = true;
 
@@ -40,8 +51,38 @@
 
   environment.systemPackages = with pkgs; [
     trayscale
+    cifs-utils
   ];  
 
   programs.mtr.enable = true;
 
+  #Samba
+  services.samba = {
+    enable = true;
+    
+    # You will still need to set up the user accounts to begin with:
+    # $ sudo smbpasswd -a yourusername
+
+    # This adds to the [global] section:
+    extraConfig = ''
+      browseable = yes
+      smb encrypt = required
+    '';
+
+    shares = {
+      homes = {
+        browseable = "no";  # note: each home will be browseable; the "homes" share will not.
+        "read only" = "no";
+        "guest ok" = "no";
+      };
+    };
+  };
+  
+  
+  
+  # Curiously, `services.samba` does not automatically open
+  # the needed ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 445 139 ];
+  networking.firewall.allowedUDPPorts = [ 137 138 ];
+  
 } 
