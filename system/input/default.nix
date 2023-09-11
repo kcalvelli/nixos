733f6d32cefc75324376f3e81baff2bf174b5ff7
keith <keith@calvelli.dev>
2023-09-11 08:25:01 -0400

    MX Master 3s

--- system/input/default.nix
+++ system/input/default.nix
@@ -15,90 +15,163 @@
 
   # Configuration for logiops
   environment.etc."logid.cfg".text = ''
-    devices: ({
-      name: "MX Vertical Advanced Ergonomic Mouse";
+devices: ({
+  name: "Wireless Mouse MX Master 3";
 
-      dpi: 1500;
+  smartshift: {
+    on: true;
+    threshold: 15;
+  };
 
-      buttons: (
-      // Forward button
-      {
-        cid: 0x56;
-        action = {
-          type: "Gestures";
-          gestures: (
-            {
-              direction: "None";
-              mode: "OnRelease";
-              action = {
-                type: "Keypress";
-                keys: [ "KEY_FORWARD" ];
-              }
-            },
+  hiresscroll: {
+    hires: true;
+    invert: false;
+    target: false;
+  };
 
-            {
-              direction: "Up";
-              mode: "OnRelease";
-              action = {
-                type: "Keypress";
-                keys: [ "KEY_PLAYPAUSE" ];
-              }
-            },
+  dpi: 1500; // max=4000
 
-            {
-              direction: "Down";
-              mode: "OnRelease";
-              action = {
-                type: "Keypress";
-                keys: [ "KEY_LEFTMETA" ];
-              }
-            },
-
-            {
-              direction: "Right";
-              mode: "OnRelease";
-              action = {
-                type: "Keypress";
-                keys: [ "KEY_NEXTSONG" ];
-              }
-            },
-
-            {
-              direction: "Left";
-              mode: "OnRelease";
-              action = {
-                type: "Keypress";
-                keys: [ "KEY_PREVIOUSSONG" ];
-              }
-            }
-          );
-        };
-      },
-
-      // Back button
-      {
-        cid: 0x53;
-        action = {
-          type: "Gestures";
-          gestures: (
-            {
-              direction: "None";
-              mode: "OnRelease";
-              action = {
-                type: "Keypress";
-                keys: [ "KEY_BACK" ];
-              }
-            }
-          );
-        };
-      },
-	
-    // Top button
+  buttons: (
+    // Forward button
     {
-      cid: 0xfd;
+      cid: 0x56;
       action = {
         type: "Gestures";
         gestures: (
+          {
+            direction: "None";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: [ "KEY_FORWARD" ];
+            }
+          },
+
+          {
+            direction: "Up";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: [ "KEY_PLAYPAUSE" ];
+            }
+          },
+
+          {
+            direction: "Down";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: [ "KEY_LEFTMETA" ];
+            }
+          },
+
+          {
+            direction: "Right";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: [ "KEY_NEXTSONG" ];
+            }
+          },
+
+          {
+            direction: "Left";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: [ "KEY_PREVIOUSSONG" ];
+            }
+          }
+        );
+      };
+    },
+
+    // Back button
+    {
+      cid: 0x53;
+      action = {
+        type: "Gestures";
+        gestures: (
+          {
+            direction: "None";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: [ "KEY_BACK" ];
+            }
+          }
+        );
+      };
+    },
+
+    // Gesture button (hold and move)
+    {
+      cid: 0xc3;
+      action = {
+        type: "Gestures";
+        gestures: (
+          {
+            direction: "None";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: [ "KEY_LEFTMETA" ]; // super key
+            }
+          },
+
+          {
+            direction: "Right";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: [ "KEY_LEFTMETA", "KEY_RIGHT" ]; // snap window to right
+            }
+          },
+
+          {
+            direction: "Left";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: [ "KEY_LEFTMETA", "KEY_LEFT" ];
+            }
+		  },
+
+		  {
+            direction: "Up";
+            mode: "onRelease";
+            action = {
+              type: "Keypress";
+              keys: [ "KEY_LEFTMETA", "KEY_UP" ]; // maximize window
+            }
+		  },
+		  
+		  {
+            direction: "Down";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: [ "KEY_LEFTMETA", "KEY_DOWN" ]; // minimize window
+            }
+          }
+        );
+      };
+    },
+	
+    // Top button
+    {
+      cid: 0xc4;
+      action = {
+        type: "Gestures";
+        gestures: (
+          {
+            direction: "None";
+            mode: "OnRelease";
+            action = {
+              type: "ToggleSmartShift";
+            }
+          },
+
           {
             direction: "Up";
             mode: "OnRelease";
@@ -119,7 +192,7 @@
         );
       };
     }
-  );      
-    });
+  );
+});
   '';
 }
