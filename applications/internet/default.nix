880fb71906abb751fe8758e70b848b0afecf6281
keith <keith@calvelli.dev>
2023-10-17 08:02:12 -0400

    Added captive browser

--- applications/internet/default.nix
+++ applications/internet/default.nix
@@ -12,5 +12,10 @@
    # discord
     google-chrome
   ]; 
-    
+
+  programs.captive-browser = {
+    enable = true;
+    interface = "wlp2s0";
+    browser = ''brave --user-data-dir=$HOME/.config/brave-captive --proxy-server="socks5://$PROXY" --host-resolver-rules="MAP * ~NOTFOUND , EXCLUDE localhost" --no-first-run --new-window --incognito http://cache.nixos.org/'';
+  };    
  }
