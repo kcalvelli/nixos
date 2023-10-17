52794625dfb5ca05ccb769a82ca7be516956fdc9
keith <keith@calvelli.dev>
2023-10-17 11:44:06 -0400

    Thought I removed captive-browser

--- applications/internet/default.nix
+++ applications/internet/default.nix
@@ -12,10 +12,4 @@
    # discord
     google-chrome
   ]; 
-
-  programs.captive-browser = {
-    enable = true;
-    interface = "wlp2s0";
-    browser = ''brave --user-data-dir=$HOME/.config/brave-captive --proxy-server="socks5://$PROXY" --host-resolver-rules="MAP * ~NOTFOUND , EXCLUDE localhost" --no-first-run --new-window --incognito http://cache.nixos.org/'';
-  };    
  }
