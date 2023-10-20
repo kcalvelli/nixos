610f1e5b9ecaab0cb3ab3adcb8ab126339078dfc
keith <keith@calvelli.dev>
2023-10-20 08:14:19 -0400

    Removed user

--- users/john/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-{
- # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.john = {
-    isNormalUser = true;
-    description = "John Calvelli";
-    extraGroups = [ "networkmanager" "wheel" "kvm" "input" "disk""libvirtd" ];
-  };
-}
