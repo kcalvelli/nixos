28d1d233c4eb3f7c89a9b3374980ec220037df7f
keith <keith@calvelli.dev>
2023-10-31 12:31:24 -0400

    All the akonadis

--- applications/kde-pim/default.nix
+++ applications/kde-pim/default.nix
@@ -5,6 +5,16 @@
   ] ++ (with pkgs.libsForQt5; [
     kdepim-runtime
     merkuro
+    akonadi
+    akonadi-mime
+    akonadi-notes
+    akonadiconsole
+    akonadi-search
+    akonadi-contacts
+    akonadi-calendar
+    akonadi-calendar-tools
+    kdepim-addons
+    kdepim-runtime
   ]);
   
 }
