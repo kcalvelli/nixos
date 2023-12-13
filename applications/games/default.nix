8014531fde3deac51972406daf01ce1b75093be8
Keith Calvelli <keith@calvelli.dev>
2023-12-13 13:02:49 -0500

    Added gamescope session to display manager

--- applications/games/default.nix
+++ applications/games/default.nix
@@ -1,7 +1,10 @@
 { inputs, config, pkgs, ... }:
 {
     programs = {
-        steam.enable = true;
+        steam = {
+            enable = true;
+            gamescopeSession.enable = true;    
+        };
     };
 
     environment.systemPackages = with pkgs; [
