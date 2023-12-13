d1377b7696827ada67ade6770af47b44d2104a52
Keith Calvelli <keith@calvelli.dev>
2023-12-13 18:09:14 -0500

    Removed gamescope session - buggy

--- applications/games/default.nix
+++ applications/games/default.nix
@@ -3,7 +3,7 @@
     programs = {
         steam = {
             enable = true;
-            gamescopeSession.enable = true;    
+    #        gamescopeSession.enable = true;    
         };
         gamescope = {
             enable = true;
