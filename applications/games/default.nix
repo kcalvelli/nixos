672c54f7d5f41fea25bec6374e97048b40937ab8
Keith Calvelli <keith@calvelli.dev>
2023-12-20 11:02:30 -0500

    Experimenting with game configs

--- applications/games/default.nix
+++ applications/games/default.nix
@@ -1,17 +1,6 @@
 { inputs, config, pkgs, ... }:
 {
-    programs = {
-        steam = {
-            enable = true;
-        };
-        gamescope = {
-            enable = true;
-        };
-    };
-
-    environment.systemPackages = with pkgs; [
-        superTuxKart
-        gamemode
-        mangohud
-    ];
+  imports = [
+    ./steam.nix
+  ];
 } 
