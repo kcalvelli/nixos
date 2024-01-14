e1061360c2fe6f7a3bc60b3dc5c300347f99b42c
keith <keith@calvelli.dev>
2024-01-13 22:32:40 -0500

    Removed hyprland, added cachix public

--- hosts/pangolin/default.nix
+++ hosts/pangolin/default.nix
@@ -27,11 +27,6 @@
       
     ];
 
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
-
   networking = { 
     hostName = "pangolin"; # Define your hostname.
   };
