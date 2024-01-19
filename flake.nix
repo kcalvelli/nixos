5ed2d61009b692cb4f9be232f3eb87e47a47312f
Keith Calvelli <keith@calvelli.dev>
2024-01-19 11:46:57 -0500

    Gave up

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
           specialArgs = { inherit inputs outputs; };
         };
         office = lib.nixosSystem {
-          modules = [ ./hosts/office inputs.kde2nix.nixosModules.default ./modules/programs/kdepim.nix ];
+          modules = [ ./hosts/office inputs.kde2nix.nixosModules.default ];
           specialArgs = { inherit inputs outputs; };
         };
       };
--- system/desktop/kde/gear.nix
+++ system/desktop/kde/gear.nix
@@ -18,6 +18,26 @@
      pkgs.kdePackages.kio-gdrive
      pkgs.kdePackages.kio-zeroconf
 
+     #kdepim
+     pkgs.kdeApplications.kmail
+     pkgs.kdeApplications.kmail-account-wizard
+     pkgs.kdeApplications.kmailtransport
+
+     pkgs.kdeApplications.merkuro
+     pkgs.kdeApplications.kleopatra
+
+     pkgs.kdeApplications.calendarsupport
+     pkgs.kdeApplications.akonadi-calendar
+     pkgs.kdeApplications.akonadi-calendar-tools
+     pkgs.kdeApplications.akonadi-contacts
+     pkgs.kdeApplications.akonadi-import-wizard
+     pkgs.kdeApplications.akonadi-mime
+     pkgs.kdeApplications.akonadiconsole
+     pkgs.kdeApplications.akonadi-notes
+     pkgs.kdeApplications.akonadi-search
+     pkgs.kdeApplications.akonadi
+     pkgs.kdeApplications.kdepim-addons
+     pkgs.kdeApplications.kdepim-runtime
     ] ++ ( with pkgs.libsForQt5; [
     utterly-round-plasma-style
     #signond
