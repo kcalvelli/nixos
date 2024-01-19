9572f2b52c868527508892139ade450276738fac
Keith Calvelli <keith@calvelli.dev>
2024-01-19 12:01:40 -0500

    Not giving up yet

--- system/desktop/kde/gear.nix
+++ system/desktop/kde/gear.nix
@@ -18,26 +18,7 @@
      pkgs.kdePackages.kio-gdrive
      pkgs.kdePackages.kio-zeroconf
 
-     #kdepim
-     pkgs.kdeApplications.kmail
-     pkgs.kdeApplications.kmail-account-wizard
-     pkgs.kdeApplications.kmailtransport
 
-     pkgs.kdeApplications.merkuro
-     pkgs.kdeApplications.kleopatra
-
-     pkgs.kdeApplications.calendarsupport
-     pkgs.kdeApplications.akonadi-calendar
-     pkgs.kdeApplications.akonadi-calendar-tools
-     pkgs.kdeApplications.akonadi-contacts
-     pkgs.kdeApplications.akonadi-import-wizard
-     pkgs.kdeApplications.akonadi-mime
-     pkgs.kdeApplications.akonadiconsole
-     pkgs.kdeApplications.akonadi-notes
-     pkgs.kdeApplications.akonadi-search
-     pkgs.kdeApplications.akonadi
-     pkgs.kdeApplications.kdepim-addons
-     pkgs.kdeApplications.kdepim-runtime
 
      #For spam
      bogofilter
