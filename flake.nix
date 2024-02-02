ab922c60c314fca72c25db7c1c99ad0ce92e02ff
Keith Calvelli <keith@calvelli.dev>
2024-02-02 10:00:26 -0500

    Changing everything

--- flake.nix
+++ flake.nix
@@ -1,64 +1,71 @@
 {
   description = "Keith Calvelli's NixOS config";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
+#  nixConfig = {
+#    extra-substituters = [
+#      "https://nix-community.cachix.org"
+#    ];
+#    extra-trusted-public-keys = [
+#      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+#    ];
+#  };
 
   inputs = {
-    # Nixpkgs
     #nixpkgs.url = "github:nixos/nixpkgs/23.11";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
-
-    # Nixos hardware
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
     kde2nix.url = "github:nix-community/kde2nix/main";
-
-   # home-manager = {
-   #   url = "github:nix-community/home-manager/master";
-   #   inputs.nixpkgs.follows =
-   #     "nixpkgs"; # Use system packages list where available
-   # };    
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-utils.inputs.systems.follows = "systems";
+    systems.url = "github:nix-systems/x86_64-linux";     
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";       
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    devshell.url = "github:numtide/devshell";  
 
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, kde2nix, home-manager, ... }@inputs:
-    let
-      inherit (self) outputs;
-      lib = nixpkgs.lib;
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
-      pkgsFor = lib.genAttrs systems (system: import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      });  
+  #outputs = { self, nixpkgs, nixos-hardware, kde2nix, home-manager, ... }@inputs:
+  outputs = inputs @ { flake-parts, systems, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+      imports = [
+        ./pkgs
+        ./nixosConfigurations
+      ];
+    };
 
-    in
-    {
-      inherit lib;
+  #  let
+  #    inherit (self) outputs;
+  #    lib = nixpkgs.lib;
+  #    systems = [ "x86_64-linux" "aarch64-linux" ];
+  #    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+  #    pkgsFor = lib.genAttrs systems (system: import nixpkgs {
+  #      inherit system;
+  #      config.allowUnfree = true;
+  #    });  
+
+   # in
+   # {
+   #   inherit lib;
 
       #nixosModules = import ./modules;
-      packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
-      devShells = forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
-      overlays = import ./overlays { inherit inputs outputs; };
+   #   packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
+   #   devShells = forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
+   #   overlays = import ./overlays { inherit inputs outputs; };
 
-      nixosConfigurations = {
-        pangolin = lib.nixosSystem {
-          modules = [ ./hosts/pangolin inputs.kde2nix.nixosModules.default ];
-          specialArgs = { inherit inputs outputs; };
-        };
-        office = lib.nixosSystem {
-          modules = [ ./hosts/office inputs.kde2nix.nixosModules.default  ];
-          specialArgs = { inherit inputs outputs; };
-        };
-      };
-    };
+#      nixosConfigurations = {
+#        pangolin = lib.nixosSystem {
+#          modules = [ ./hosts/pangolin inputs.kde2nix.nixosModules.default ];
+#          specialArgs = { inherit inputs outputs; };
+#        };
+#        office = lib.nixosSystem {
+#          modules = [ ./hosts/office inputs.kde2nix.nixosModules.default  ];
+#          specialArgs = { inherit inputs outputs; };
+#        };
+#      };
+  #  };
 
     #homeConfigurations = {
     #  "keith" = home-manager.lib.homeManagerConfiguration {
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,34 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 
-{ pkgs ? import <nixpkgs> { } }: rec {
+#{ pkgs ? import <nixpkgs> { } }: rec {
+#
+#  # Brave Nightly
+#    brave-browser-nightly = pkgs.callPackage ./brave-browser-nightly { };
+#  # Brave
+#    brave = pkgs.callPackage ./brave { }; 
+#}
+{ self, inputs, ... }: {
+  perSystem = { system, pkgs, ... }: {
+   packages = {
+    inherit (pkgs)
+      brave
+      ;
+   };
 
-  # Brave Nightly
-    brave-browser-nightly = pkgs.callPackage ./brave-browser-nightly { };
-  # Brave
-    brave = pkgs.callPackage ./brave { }; 
+   _module.args.pkgs = import inputs.nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+      overlays = [
+        self.overlays.default
+      ];
+   };
+  };
+
+  flake.overlays.default = _final: prev: {
+    brave = prev.callPackage ./brave { };
+  };
 }
 
