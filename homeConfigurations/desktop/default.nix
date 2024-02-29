5f3b4e9e9c151ad195b84a49678e3408ff471e77
Keith Calvelli <keith@calvelli.dev>
2024-02-29 11:19:57 -0500

    Native messaging host for Vivaldi

--- homeConfigurations/desktop/default.nix
+++ homeConfigurations/desktop/default.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+    #Native Messaging Host for Brave
     home.file.".config/BraveSoftware/Brave-Browser/NativeMessagingHosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
     home.file.".config/BraveSoftware/Brave-Browser-Nightly/NativeMessagingHosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
+
+    #Native Messaging Host for Vivaldi
+    home.file.".config/vivaldi/NativeMessagingHosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";    
 }
