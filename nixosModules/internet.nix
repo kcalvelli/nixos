cee598a07440c4b07a5ba7fcd010a94135394bd5
Keith Calvelli <keith@calvelli.dev>
2024-03-03 19:26:37 -0500

    Removed vivaldi

--- nixosModules/internet.nix
+++ nixosModules/internet.nix
@@ -3,9 +3,8 @@
   environment.systemPackages = with pkgs; [
     inputs.self.packages.${pkgs.system}.brave-qt
     #inputs.self.packages.${pkgs.system}.brave-browser-nightly
-    inputs.self.packages.${pkgs.system}.vivaldi
+    #inputs.self.packages.${pkgs.system}.vivaldi
     nextcloud-client
-    telegram-desktop
-    #mailspring   
+    telegram-desktop   
   ];
 }
