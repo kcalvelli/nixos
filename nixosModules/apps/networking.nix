99473f9098339816a2ab2c0cbd6bfdce67fc9147
Keith Calvelli <keith@calvelli.dev>
2024-03-26 08:41:19 -0400

    Flake update & move back to nixos brave from custom

--- nixosModules/apps/networking.nix
+++ nixosModules/apps/networking.nix
@@ -1,10 +1,10 @@
 { inputs, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    inputs.self.packages.${pkgs.system}.brave-qt
+    #inputs.self.packages.${pkgs.system}.brave-qt
     #inputs.self.packages.${pkgs.system}.brave-browser-nightly
     #inputs.self.packages.${pkgs.system}.vivaldi
-    #brave
+    brave
     nextcloud-client
     telegram-desktop   
     thunderbird
