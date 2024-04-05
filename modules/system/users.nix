90e4128e220cf26bc60e88270b75847eee5e8152
Keith Calvelli <keith@calvelli.dev>
2024-04-05 08:53:44 -0400

    Remove unused user

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -7,26 +7,19 @@
       isNormalUser = true;
       description = "Keith Calvelli";
       extraGroups = [ "networkmanager" "wheel" "kvm" "input" "disk""libvirtd" "plugdev" "libvirtd" ];
-    };
-    calvelli = {
-      isNormalUser = true;
-      description = "Keith Calvelli";
-      extraGroups = [ "networkmanager" "wheel" "kvm" "input" "disk""libvirtd" "plugdev" "libvirtd" ];
-    };        
+    };       
   };
 
   home-manager.users = {
     keith = {
-      imports = [ inputs.self.homeModules.desktop ];
+      imports = [ 
+        inputs.self.homeModules.desktop
+        inputs.self.homeModules.virtualisation 
+      ];
       home.stateVersion = "24.05";
       home.homeDirectory = "/home/keith";
       home.username = "keith";      
-    };
-    calvelli = {
-      imports = [ inputs.self.homeModules.desktop ];
-      home.stateVersion = "24.05";
-      home.homeDirectory = "/home/calvelli";
-      home.username = "calvelli";      
+    };     
     };     
   };
 }
