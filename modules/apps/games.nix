3662a5ee55b3726ba0d2d1b9d5a6e6a69cea9724
Keith Calvelli <keith@calvelli.dev>
2024-04-13 10:35:23 -0400

    Simplified steam

--- modules/apps/games.nix
+++ modules/apps/games.nix
@@ -1,26 +1,12 @@
 { pkgs, lib, config, ... }:
-let
-  steam-with-pkgs = pkgs.steam.override {
-    extraPkgs = pkgs: with pkgs; [
-      xorg.libXcursor
-      xorg.libXi
-      xorg.libXinerama
-      xorg.libXScrnSaver
-      libpng
-      libpulseaudio
-      libvorbis
-      stdenv.cc.cc.lib
-      libkrb5
-      keyutils
-      gamescope
-      mangohud
-    ];
-  };
-
-in
 {
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+  };
+  programs.gamemode.enable = true;
+
   environment.systemPackages = with pkgs; [ 
-    steam-with-pkgs
     gamescope
     mangohud
     protontricks
