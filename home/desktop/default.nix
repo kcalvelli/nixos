cabf41e6eaa4ec46b80207dd82326caf1442b9c1
Keith Calvelli <keith@calvelli.dev>
2024-05-01 12:17:54 -0400

    Trying to get autostart working.

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,17 +1,16 @@
 { pkgs, ... }:
 
 {
-  systemd.user.targets.autostart-cosmic = {
-    Unit.Wants = [ "xdg-desktop-autostart.target" ]; 
-    Unit.After = [ "default.target" ];
-  };
-
-  home.packages = [
-    #Here be packages
-    (pkgs.writeShellScriptBin "autostart" ''
-      systemctl start --user autostart-cosmic.target
-      '')
-  ];  
+ # systemd.user.targets.autostart-cosmic = {
+ #   Unit.Wants = [ "xdg-desktop-autostart.target" ]; 
+ #   Unit.After = [ "default.target" ];
+ # };
+ # home.packages = [
+ #   #Here be packages
+ #   (pkgs.writeShellScriptBin "autostart" ''
+ #     systemctl start --user autostart-cosmic.target
+ #     '')
+ # ];  
 
    #Native Messaging Host for Brave - Enable if running plasma
     #home.file.".config/BraveSoftware/Brave-Browser/NativeMessagingHosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json"; 
