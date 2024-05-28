703b6ced4a918948d083a970682404f519f45c37
Keith Calvelli <keith@calvelli.dev>
2024-05-28 12:32:23 -0400

    Moving system76_acpi module to initrd

--- modules/system/hardware/system76.nix
+++ modules/system/hardware/system76.nix
@@ -16,8 +16,8 @@
     blacklistedKernelModules = [ "psmouse" ];
     loader.systemd-boot.enable = true;
     initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" "system76-io" "system76-acpi" ];
-    initrd.kernelModules = [  ];
-    kernelModules = [ "kvm-amd" "system76-acpi" ];
+    initrd.kernelModules = [ "system76-acpi" ];
+    kernelModules = [ "kvm-amd" ];
     extraModulePackages = [ ];
     #extraModprobeConfig = ''
     #options mt7921_common disable_clc=1
