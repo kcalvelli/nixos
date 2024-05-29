dc037e2ed927e95401af93d22b78392e4186eca5
Keith Calvelli <keith@calvelli.dev>
2024-05-29 11:25:43 -0400

    Flake update, scaled back gnome stuff

--- modules/desktops/cosmic.nix
+++ modules/desktops/cosmic.nix
@@ -10,19 +10,10 @@
   services.displayManager.cosmic-greeter.enable = true;
 
   services = {
-    gnome.core-utilities.enable = true;
-    tumbler.enable = true;
     gvfs.enable = true;
     acpid.enable = true;
     gnome.sushi.enable = true;
     gnome.gnome-keyring.enable = true; 
-    gnome.tracker.enable = true;
-    gnome.tracker-miners.enable = true;
-    gnome.gnome-online-accounts.enable = true;
-    gnome.gnome-settings-daemon.enable = true;
-    gnome.gnome-user-share.enable = true;
-    gnome.glib-networking.enable = true;
-    flatpak.enable = true;
   };
 
   programs = {
@@ -32,32 +23,13 @@
     seahorse.enable = true;
   };
 
-  environment.gnome.excludePackages = with pkgs; [
-    gnome.epiphany
-    gnome-text-editor
-    gnome.gnome-calendar
-    gnome.gnome-characters
-    gnome.gnome-clocks
-    gnome-console
-    gnome.gnome-contacts
-    gnome.gnome-font-viewer
-    gnome.gnome-maps
-    gnome.gnome-music
-    gnome.gnome-system-monitor
-    gnome.gnome-weather
-    gnome.totem
-    gnome.yelp
-    gnome.geary
-    gnome.gnome-color-manager
-    gnome.gnome-software
-
-  ];
-
   environment.systemPackages = with pkgs; [
     adw-gtk3   
     cosmic-tasks
     cosmic-emoji-picker
-    inputs.self.packages.${pkgs.system}.cosmic-webapps
+    #inputs.self.packages.${pkgs.system}.cosmic-webapps
+    gnome.nautilus
+    nautilus-open-any-terminal
   ];
 
   environment.sessionVariables.NAUTILUS_4_EXTENSION_DIR = "${config.system.path}/lib/nautilus/extensions-4";
