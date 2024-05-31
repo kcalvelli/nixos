b46ca1bd2e022dbec12e84ad61821b70020d6373
Keith Calvelli <keith@calvelli.dev>
2024-05-31 14:42:09 -0400

    Radeon tweaks

--- modules/system/hardware/graphics.nix
+++ modules/system/hardware/graphics.nix
@@ -7,7 +7,14 @@
 
   environment.systemPackages = with pkgs; [
     lact
+    radeontop
+    rocmPackages.rocm-smi
+    umr
   ];
+
+  boot.kernel.sysctl = {
+    "kernel.split_lock_mitigate" = 0;
+  };
 
   custom.lact = {
     enable = true;
