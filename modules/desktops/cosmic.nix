83ca7ce4fdfd55c00e9052c4168674171f622d04
Keith Calvelli <keith@calvelli.dev>
2024-06-12 12:57:43 -0400

    Using drakon64 cosmic module until gnome fix is merged

--- modules/desktops/cosmic.nix
+++ modules/desktops/cosmic.nix
@@ -6,7 +6,11 @@
   ];
 
   #Just cosmic
-  #services.desktopManager.cosmic.enable = true;
+  services.desktopManager.cosmic.enable = true;
+
+  environment.systemPackages = with pkgs; {
+    adw-gtk3
+  };
   #services.displayManager.cosmic-greeter.enable = true;
 
   #services = {
@@ -51,91 +55,91 @@
   #''];
 
   # environment packages
-  environment.etc."cosmic-comp/config.ron".source = lib.mkDefault "${pkgs.cosmic-comp}/etc/cosmic-comp/config.ron";
-  environment.pathsToLink = [ "/share/cosmic" ];
-  environment.systemPackages = utils.removePackagesByName (with pkgs; [
-    adw-gtk3
-    alsa-utils
-    cosmic-applets
-    cosmic-applibrary
-    cosmic-bg
-    cosmic-comp
-    cosmic-edit
-    cosmic-files
-    cosmic-greeter
-    cosmic-icons
-    cosmic-launcher
-    cosmic-notifications
-    cosmic-osd
-    cosmic-panel
-    cosmic-randr
-    cosmic-screenshot
-    cosmic-session
-    cosmic-settings
-    cosmic-settings-daemon
-    cosmic-term
-    cosmic-workspaces-epoch
-    gnome.adwaita-icon-theme
-    hicolor-icon-theme
-    pop-icon-theme
-    pop-launcher
-  ] ++ lib.optionals config.services.flatpak.enable [
-    cosmic-store
-  ]) config.environment.cosmic.excludePackages;
+  #environment.etc."cosmic-comp/config.ron".source = lib.mkDefault "${pkgs.cosmic-comp}/etc/cosmic-comp/config.ron";
+  #environment.pathsToLink = [ "/share/cosmic" ];
+  #environment.systemPackages = utils.removePackagesByName (with pkgs; [
+  #  adw-gtk3
+  #  alsa-utils
+  #  cosmic-applets
+  #  cosmic-applibrary
+  #  cosmic-bg
+  #  cosmic-comp
+  #  cosmic-edit
+  #  cosmic-files
+  #  cosmic-greeter
+  #  cosmic-icons
+  #  cosmic-launcher
+  #  cosmic-notifications
+  #  cosmic-osd
+  #  cosmic-panel
+  #  cosmic-randr
+  #  cosmic-screenshot
+  #  cosmic-session
+  #  cosmic-settings
+  #  cosmic-settings-daemon
+  #  cosmic-term
+  #  cosmic-workspaces-epoch
+  #  gnome.adwaita-icon-theme
+  #  hicolor-icon-theme
+  #  pop-icon-theme
+  #  pop-launcher
+  #] ++ lib.optionals config.services.flatpak.enable [
+  #  cosmic-store
+  #]) config.environment.cosmic.excludePackages;
 
   # xdg portal packages and config
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-cosmic
-    ] ++ lib.optional (!config.services.xserver.desktopManager.gnome.enable) pkgs.xdg-desktop-portal-gtk; # https://github.com/lilyinstarlight/nixos-cosmic/issues/17#issuecomment-2139612801
-    configPackages = lib.mkDefault (with pkgs; [
-      xdg-desktop-portal-cosmic
-    ]);
-  };
+  #xdg.portal = {
+  #  enable = true;
+  #  extraPortals = with pkgs; [
+  #    xdg-desktop-portal-cosmic
+  #  ] ++ lib.optional (!config.services.xserver.desktopManager.gnome.enable) pkgs.xdg-desktop-portal-gtk; # https://github.com/lilyinstarlight/nixos-cosmic/issues/17#issuecomment-2139612801
+  #  configPackages = lib.mkDefault (with pkgs; [
+  #    xdg-desktop-portal-cosmic
+  #  ]);
+  #};
 
   # fonts
-  fonts.packages = utils.removePackagesByName (with pkgs; [
-    fira
-  ]) config.environment.cosmic.excludePackages;
+  #fonts.packages = utils.removePackagesByName (with pkgs; [
+  #  fira
+  #]) config.environment.cosmic.excludePackages;
 
   # required features
-  hardware.opengl.enable = true;
-  services.libinput.enable = true;
-  xdg.mime.enable = true;
-  xdg.icons.enable = true;
+  #hardware.opengl.enable = true;
+  #services.libinput.enable = true;
+  #xdg.mime.enable = true;
+  #xdg.icons.enable = true;
 
   # optional features
-  hardware.bluetooth.enable = lib.mkDefault true;
-  services.acpid.enable = lib.mkDefault true;
-  services.pipewire = {
-    enable = lib.mkDefault true;
-    alsa.enable = lib.mkDefault true;
-    pulse.enable = lib.mkDefault true;
-  };
-  services.gvfs.enable = lib.mkDefault true;
-  networking.networkmanager.enable = lib.mkDefault true;
-  services.gnome.gnome-keyring.enable = lib.mkDefault true;
+  #hardware.bluetooth.enable = lib.mkDefault true;
+  #services.acpid.enable = lib.mkDefault true;
+  #services.pipewire = {
+  #  enable = lib.mkDefault true;
+  #  alsa.enable = lib.mkDefault true;
+  #  pulse.enable = lib.mkDefault true;
+  #};
+  #services.gvfs.enable = lib.mkDefault true;
+  #networking.networkmanager.enable = lib.mkDefault true;
+  #services.gnome.gnome-keyring.enable = lib.mkDefault true;
 
   # general graphical session features
-  programs.dconf.enable = lib.mkDefault true;
+  #programs.dconf.enable = lib.mkDefault true;
 
   # required dbus services
-  services.accounts-daemon.enable = true;
-  services.upower.enable = true;
-  services.power-profiles-daemon.enable = lib.mkDefault (!config.hardware.system76.power-daemon.enable);
-  security.polkit.enable = true;
+  #services.accounts-daemon.enable = true;
+  #services.upower.enable = true;
+  #services.power-profiles-daemon.enable = lib.mkDefault (!config.hardware.system76.power-daemon.enable);
+  #security.polkit.enable = true;
 
   # session packages
-  services.displayManager.sessionPackages = with pkgs; [ cosmic-session ];
-  systemd.packages = with pkgs; [ cosmic-session ];
-  # TODO: remove when upstream has XDG autostart support
-  systemd.user.targets.cosmic-session = {
-    wants = [ "xdg-desktop-autostart.target" ];
-    before = [ "xdg-desktop-autostart.target" ];
-  };
+  #services.displayManager.sessionPackages = with pkgs; [ cosmic-session ];
+  #systemd.packages = with pkgs; [ cosmic-session ];
+  ## TODO: remove when upstream has XDG autostart support
+  #systemd.user.targets.cosmic-session = {
+  #  wants = [ "xdg-desktop-autostart.target" ];
+  #  before = [ "xdg-desktop-autostart.target" ];
+  #};
 
-  # required for screen locker
-  security.pam.services.cosmic-greeter = {};
+  ## required for screen locker
+  #security.pam.services.cosmic-greeter = {};
       
 }
