218bfe3c7475b444044cf45d5ba91a20314bc7ad
Keith Calvelli <keith@calvelli.dev>
2024-06-19 17:12:50 -0400

    Renamed packages to pkgs

--- modules/apps/development.nix
+++ modules/apps/development.nix
@@ -1,13 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
-   #packages.x86_64-linux.default = fenix.packages.x86_64-linux.default.toolchain;
-
-
    environment.systemPackages = with pkgs; [
      # Rolling with Zed for a minute
      zed-editor
      #vscode
      #jetbrains.rust-rover
+     inputs.self.packages.${pkgs.system}.cursor
    ];
    programs.direnv.enable = true;
    services.lorri.enable = true;
