5e7e96097d4faaffa3c301bfbfc07cb37a2e604b
Keith Calvelli <keith@calvelli.dev>
2024-06-30 08:09:46 -0400

    Simplify repo structure

--- modules/default.nix
+++ modules/default.nix
@@ -1,4 +1,18 @@
 { ... }:
 {
-  imports = [ ./features ];
+  flake.nixosModules = {
+
+    config = ./config;
+    core = ./core;
+    development = ./development;
+    gaming = ./gaming;
+    multimedia = ./multimedia;
+    networking = ./networking;
+    printing = ./printing;
+    productivity = ./productivity;
+    utils = ./utils;
+    system76 = ./hardware/system76.nix;
+    msi = ./hardware/msi.nix;
+    virtualisation = ./virtualisation;
+  };
 }
