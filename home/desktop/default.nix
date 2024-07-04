5e21d995d8e599aaae4cd4cdc11c8a29699c08e7
Keith Calvelli <keith@calvelli.dev>
2024-07-04 15:26:02 -0400

    PWA desktop files

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -42,7 +42,7 @@
   };
 
   home.file = {
-   ".local/share/applications/brave-ejhkdoiecgkmdpomoahkdihbcldkgjci-Default".text =
+   ".local/share/applications/brave-ejhkdoiecgkmdpomoahkdihbcldkgjci-Default.desktop".text =
     ''
     #!/usr/bin/env xdg-open
     [Desktop Entry]
@@ -54,5 +54,150 @@
     Icon=brave-ejhkdoiecgkmdpomoahkdihbcldkgjci-Default
     StartupWMClass=crx_ejhkdoiecgkmdpomoahkdihbcldkgjci    
     '';  
+  };  
+  
+  home.file = {
+   ".local/share/applications/brave-faolnafnngnfdaknnbpnkhgohbobgegn-Default.desktop".text =
+    ''
+    #!/usr/bin/env xdg-open
+    [Desktop Entry]
+    Version=1.0
+    Terminal=false
+    Type=Application
+    Name=Outlook (PWA)
+    MimeType=x-scheme-handler/mailto
+    Exec=/brave --profile-directory=Default --app-id=faolnafnngnfdaknnbpnkhgohbobgegn %U
+    Icon=brave-faolnafnngnfdaknnbpnkhgohbobgegn-Default
+    StartupWMClass=crx_faolnafnngnfdaknnbpnkhgohbobgegn
+    Actions=New-event;New-message;Open-calendar
+    
+    [Desktop Action New-event]
+    Name=New event
+    Exec=brave --profile-directory=Default --app-id=faolnafnngnfdaknnbpnkhgohbobgegn --app-launch-url-for-shortcuts-menu-item=https://outlook.office.com/calendar/deeplink/compose
+    
+    [Desktop Action New-message]
+    Name=New message
+    Exec=brave --profile-directory=Default --app-id=faolnafnngnfdaknnbpnkhgohbobgegn --app-launch-url-for-shortcuts-menu-item=https://outlook.office.com/mail/deeplink/compose
+    
+    [Desktop Action Open-calendar]
+    Name=Open calendar
+    Exec=brave --profile-directory=Default --app-id=faolnafnngnfdaknnbpnkhgohbobgegn --app-launch-url-for-shortcuts-menu-item=https://outlook.office.com/calendar   
+    '';  
+  };  
+
+  home.file = {
+   ".local/share/applications/brave-hpfldicfbfomlpcikngkocigghgafkph-Default.desktop".text =
+    ''
+    #!/usr/bin/env xdg-open
+    [Desktop Entry]
+    Version=1.0
+    Terminal=false
+    Type=Application
+    Name=Messages
+    Exec=brave --profile-directory=Default --app-id=hpfldicfbfomlpcikngkocigghgafkph
+    Icon=brave-hpfldicfbfomlpcikngkocigghgafkph-Default
+    StartupWMClass=crx_hpfldicfbfomlpcikngkocigghgafkph
+    '';  
+  }; 
+
+  home.file = {
+   ".local/share/applications/brave-kjgfgldnnfoeklkmfkjfagphfepbbdan-Default.desktop".text =
+    ''
+    #!/usr/bin/env xdg-open
+    [Desktop Entry]
+    Version=1.0
+    Terminal=false
+    Type=Application
+    Name=Google Meet
+    Exec=brave --profile-directory=Default --app-id=kjgfgldnnfoeklkmfkjfagphfepbbdan
+    Icon=brave-kjgfgldnnfoeklkmfkjfagphfepbbdan-Default
+    StartupWMClass=crx_kjgfgldnnfoeklkmfkjfagphfepbbdan
+    '';  
+  };    
+
+  home.file = {
+   ".local/share/applications/brave-mdpkiolbdkhdjpekfbkbmhigcaggjagi-Default.desktop".text =
+    ''
+    #!/usr/bin/env xdg-open
+    [Desktop Entry]
+    Version=1.0
+    Terminal=false
+    Type=Application
+    Name=Google Chat
+    Exec=brave --profile-directory=Default --app-id=mdpkiolbdkhdjpekfbkbmhigcaggjagi
+    Icon=brave-mdpkiolbdkhdjpekfbkbmhigcaggjagi-Default
+    StartupWMClass=crx_mdpkiolbdkhdjpekfbkbmhigcaggjagi
+    '';  
+  };   
+
+  home.file = {
+   ".local/share/applications/brave-mnhkaebcjjhencmpkapnbdaogjamfbcj-Default.desktop".text =
+    ''
+    #!/usr/bin/env xdg-open
+    [Desktop Entry]
+    Version=1.0
+    Terminal=false
+    Type=Application
+    Name=Google Maps
+    Exec=brave --profile-directory=Default --app-id=mnhkaebcjjhencmpkapnbdaogjamfbcj
+    Icon=brave-mnhkaebcjjhencmpkapnbdaogjamfbcj-Default
+    StartupWMClass=crx_mnhkaebcjjhencmpkapnbdaogjamfbcj
+    '';  
+  };    
+
+  home.file = {
+   ".local/share/applications/brave-ncmjhecbjeaamljdfahankockkkdmedg-Default.desktop".text =
+    ''
+    #!/usr/bin/env xdg-open
+    [Desktop Entry]
+    Version=1.0
+    Terminal=false
+    Type=Application
+    Name=Google Photos
+    Exec=brave --profile-directory=Default --app-id=ncmjhecbjeaamljdfahankockkkdmedg
+    Icon=brave-ncmjhecbjeaamljdfahankockkkdmedg-Default
+    StartupWMClass=crx_ncmjhecbjeaamljdfahankockkkdmedg
+    '';  
+  };  
+
+  home.file = {
+   ".local/share/applications/brave-oiocllghmdadfpahmllbbhkgjfmaidmm-Default.desktop".text =
+    ''
+    #!/usr/bin/env xdg-open
+    [Desktop Entry]
+    Version=1.0
+    Terminal=false
+    Type=Application
+    Name=Microsoft Teams
+    Exec=brave --profile-directory=Default --app-id=oiocllghmdadfpahmllbbhkgjfmaidmm
+    Icon=brave-oiocllghmdadfpahmllbbhkgjfmaidmm-Default
+    StartupWMClass=crx_oiocllghmdadfpahmllbbhkgjfmaidmm
+    '';  
+  };      
+
+  home.file = {
+    ".local/share/applications/code.desktop".text = 
+    ''
+    #!/usr/bin/env xdg-open
+    [Desktop Entry]
+    Actions=new-empty-window
+    Categories=Utility;TextEditor;Development;IDE
+    Comment=Code Editing. Redefined.
+    Exec=code %F
+    GenericName=Text Editor
+    Icon=vscode
+    Keywords=vscode
+    MimeType=text/plain;inode/directory
+    Name=Visual Studio Code
+    StartupNotify=true
+    StartupWMClass=Code
+    Type=Application
+    Version=1.4
+    
+    [Desktop Action new-empty-window]
+    Exec=code --new-window %F
+    Icon=vscode
+    Name=New Empty Window
+    '';
   };  
 }
