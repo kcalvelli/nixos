58897a0d0781d5e94e6cbbdc79488a5b7d620745
Keith Calvelli <keith@calvelli.dev>
2024-07-03 22:54:28 -0400

    gamescope

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -2,7 +2,6 @@
 {
   imports = [
     inputs.nix-gaming.nixosModules.platformOptimizations
-    inputs.Jovian-NixOS.nixosModules.default
   ];
 
   programs.steam = {
@@ -14,7 +13,10 @@
 
   programs.gamemode = {
     enable = true;
-    capSysNice = true;
+  };
+
+  programs.gamescope = {
+    enable = true;
   };
 
   environment.systemPackages = with pkgs; [
