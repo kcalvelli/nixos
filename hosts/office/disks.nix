cdd8397977e40cbb8bf48b474aaedc07a3693c0c
Keith Calvelli <keith@calvelli.dev>
2024-07-15 09:51:21 -0400

    Reinstallig office - update disks

--- hosts/office/disks.nix
+++ hosts/office/disks.nix
@@ -2,23 +2,23 @@
 
 {
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/bb3c56b8-00a5-44f6-bbe7-1956e5b37c38";
+    device = "/dev/disk/by-uuid/31ca4929-c5c0-4a5e-a4e8-d07e18806ced";
     fsType = "ext4";
   };
 
-  fileSystems."/mnt/games" = {
+  boot.initrd.luks.devices."luks-e05489aa-0fc2-4801-a3cc-c0e8498b8fa9".device = "/dev/disk/by-uuid/e05489aa-0fc2-4801-a3cc-c0e8498b8fa9";
+
+  fileSystems."/media/games" = {
     device = "/dev/disk/by-uuid/22d680fd-da6a-49fa-a2e6-3194a925f7ed";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/8371-0372";
+    device = "/dev/disk/by-uuid/3D0B-6C4A";
     fsType = "vfat";
     options = [
       "fmask=0022"
       "dmask=0022"
     ];
   };
-
-  swapDevices = [ { device = "/dev/disk/by-uuid/79edb6e5-48e1-4b34-a5a6-c01fd230a4eb"; } ];
 }
