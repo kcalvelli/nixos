1dc5d373b03bee13ce1e51434bbee975bfc9260c
Keith Calvelli <keith@calvelli.dev>
2024-07-17 16:04:37 -0400

    Modularized hardware config

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -7,6 +7,8 @@ in
      inputs.nixos-hardware.nixosModules.common-cpu-amd
      inputs.nixos-hardware.nixosModules.common-pc-ssd
      inputs.chaotic.nixosModules.default
+     inputs.lanzaboote.nixosModules.lanzaboote    
+     inputs.nixos-hardware.nixosModules.system76      
   ];
 
   options = {
@@ -16,43 +18,54 @@ in
     hardware.system76 = {
       enable = lib.mkEnableOption "System76 hardware";
     };
-  };
-
-  ### Sound
-  # Make pipewire realtime capable
-  security.rtkit.enable = true;
-
-  # Can't have pulse and pipewire 
-  hardware.pulseaudio.enable = lib.mkForce false;
-
-  ### Printing
-  services = {
-    printing = {
-      enable = true;
-      drivers = [
-        pkgs.hplip
-        pkgs.hplipWithPlugin
-      ];
+    hardware.common = {
+      enable = lib.mkEnableOption "Common hardware config";
     };
   };
 
-  ### Firmware
-  hardware = {
-    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;  
-    enableAllFirmware = true;
-  };  
-
-  ### Kernel and scheduler
-  boot.kernelPackages = pkgs.linuxPackages_cachyos;
-  #chaotic.scx.enable = true;
-  #chaotic.scx.scheduler = "scx_rusty";
-
   ### MSI motherboard system
   config = lib.mkMerge [
+    (lib.mkIf cfg.common.enable {
+      ### Sound
+      # Make pipewire realtime capable
+      security.rtkit.enable = true;
+    
+      # Can't have pulse and pipewire 
+      hardware.pulseaudio.enable = lib.mkForce false;
+    
+
+      services = {
+      ### Printing
+        printing = {
+          enable = true;
+          drivers = [
+            pkgs.hplip
+            pkgs.hplipWithPlugin
+          ];
+        };
+      ### Force power profiles daemon
+        power-profiles-daemon = {
+          enable = lib.mkForce true;
+        };
+      };
+    
+      ### Firmware
+      hardware = {
+        cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;  
+        enableAllFirmware = true;
+      };  
+    
+      
+  
+      ### Kernel and scheduler
+      boot.kernelPackages = pkgs.linuxPackages_cachyos;
+      #chaotic.scx.enable = true;
+      #chaotic.scx.scheduler = "scx_rusty";
+    })
+
     (lib.mkIf cfg.msi.enable {
-      imports = [
-        inputs.lanzaboote.nixosModules.lanzaboote
-      ];
+
+       hardware.common.enable = true;
 
        ### Logitech keyboard and mouse      
        hardware = {
@@ -103,11 +116,9 @@ in
     })
 
     ### System76 laptop (Pangolin 12)
-   (lib.mkIf cfg.system76.enable {
-      imports = [
-        inputs.nixos-hardware.nixosModules.system76
-      ];
-    
+   (lib.mkIf cfg.system76.enable {      
+      hardware.common.enable = true;
+
       boot = {
         kernelParams = [
           "ro"
@@ -135,7 +146,6 @@ in
         system76 = {
           enableAll = true;
           kernel-modules.enable = true;
-          power-daemon.enable = false;
         };
       };
     
