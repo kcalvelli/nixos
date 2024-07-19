b75179a6cf412a419893f29f341edff6c0a4eebd
Keith Calvelli <keith@calvelli.dev>
2024-07-18 22:02:31 -0400

    Flake update

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -24,6 +24,7 @@
   home-manager.useGlobalPkgs = true; # don't create another instance of nixpkgs
   home-manager.useUserPackages = true; # install user packages directly to the user's profile
 
+  #  To do - don't hardcode for a specific user.  How?
   home-manager.users = {
     keith = {
       imports = with inputs.self.homeModules; [
