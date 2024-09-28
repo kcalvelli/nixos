8a0c157a3d03ec22038ad382ee35c979b2dc1487
Keith Calvelli <keith@calvelli.dev>
2024-09-28 12:17:25 -0400

    Flake update, added some apps from nixos-cosmic

--- modules/desktop/cosmic.nix
+++ modules/desktop/cosmic.nix
@@ -10,5 +10,14 @@
   config = lib.mkIf config.cosmic.enable {
     services.desktopManager.cosmic.enable = true;   
     services.displayManager.cosmic-greeter.enable = true;   
+
+    environment.systemPackages = with pkgs; [
+      cosmic-forecast
+      cosmic-applet-emoji-selector
+      cosmic-calculator
+      cosmic-player
+      cosmic-reader
+      cosmic-tasks
+    ];
   }; 
 }
