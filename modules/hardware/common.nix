01b9509896b57f2f3228250778edbea3e2348336
Keith Calvelli <keith@calvelli.dev>
2024-11-15 15:44:33 -0500

    overlay planes patch for amdgpu

--- modules/hardware/common.nix
+++ modules/hardware/common.nix
@@ -5,6 +5,10 @@
   inputs,
   ...
 }:
+let amdgpu-kernel-module = pkgs.callPackage ./patches/amdgpu.nix {
+  kernel = config.boot.kernelPackages.kernel;
+};
+in 
 {
   imports = [
     ./boot.nix
@@ -35,5 +39,30 @@
   # Firmware
   services.fwupd.enable = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_latest; 
+  boot = {
+    kernelPackages = 
+    let
+       versionNum = "6.12.0-rc7";
+       linux_6_12_pkg = { fetchurl, buildLinux, ... } @args:
+        buildLinux (args // {
+          src = fetchurl {
+            url = "https://git.kernel.org/torvalds/t/linux-6.12-rc7.tar.gz";
+            sha256 = "sha256-hYHy81vx++0nGt1a9zz9iMKZorO+RmgRARaYdEPKo2E=";
+          };
+          dontStrip = true;
+          version = versionNum;
+          modDirVersion = versionNum;
+        } // (args.argsOverride or { }));
+      linux_6_12 = pkgs.callPackage linux_6_12_pkg {};
+    in
+    pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_6_12);
+    # Patch AMDGPU for overlay planes bug
+    extraModulePackages = [
+      (amdgpu-kernel-module.overrideAttrs (_: {
+        patches = [
+          ./patches/overlay-planes-patch_6-12-rc7.patch
+         ];
+      }))
+    ];
+  };
 }
