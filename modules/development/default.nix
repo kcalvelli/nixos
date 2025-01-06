9852db7a079991d6921718acdd488e527fe7fc70
Keith Calvelli <keith@calvelli.dev>
2025-01-06 12:23:19 -0500

    Launch fish when interactive shell is detected

--- modules/development/default.nix
+++ modules/development/default.nix
@@ -23,4 +23,15 @@
   programs.direnv.enable = true;
   services.lorri.enable = true;  
   programs.git.enable = true;   
+
+  # Launch Fish when interactive shell is detected
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
 }
