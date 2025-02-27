d299e0a75c44cd792f0b35ed6306a85426c7f0bb
Keith Calvelli <1329446+kcalvelli@users.noreply.github.com>
2025-02-27 10:39:55 -0500

    Adding github action

--- modules/development/default.nix
+++ modules/development/default.nix
@@ -7,32 +7,93 @@
 
   # Define system packages for development
   environment.systemPackages = with pkgs; [
-    devenv
-    nil
-    nixfmt-rfc-style
+    # Editor and IDE tools
     vim
+    vscode
+    windsurf
+
+    # Nix development tools
+    devenv
+    nil # Nix LSP
+    nixfmt-rfc-style
+
+    # Shell and terminal tools
     starship
     fish
-    gcc
+    bat # Better cat
+    exa # Better ls
+    ripgrep # Fast search
+    fd # Better find
+    jq # JSON processor
+    fzf # Fuzzy finder
+
+    # Version control
     github-desktop
+    git-lfs
+
+    # Build tools and compilers
+    gcc
+    gnumake
+    cmake
+    pkg-config
+
+    # Development utilities
     code-cursor
-    vscode
   ];
 
-  # Enable necessary programs and services
-  programs.direnv.enable = true;
-  services.lorri.enable = true;
-  programs.git.enable = true;
-  services.vscode-server.enable = true;
+  # Enable and configure development services
+  services = {
+    lorri.enable = true;
+    vscode-server.enable = true;
+  };
 
-  # Launch Fish when interactive shell is detected
-  programs.bash = {
-    interactiveShellInit = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
+  # Configure development programs
+  programs = {
+    direnv.enable = true;
+    
+    git = {
+      enable = true;
+      config = {
+        init.defaultBranch = "main";
+        pull.rebase = true;
+        push.autoSetupRemote = true;
+        core = {
+          editor = "vim";
+          autocrlf = "input";
+        };
+      };
+    };
+
+    # Configure starship prompt
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = true;
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[✗](bold red)";
+        };
+      };
+    };
+
+    # Configure fish shell
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set -U fish_greeting # Disable greeting
+        fish_vi_key_bindings # Use vi key bindings
+      '';
+    };
+
+    # Launch Fish when interactive shell is detected
+    bash = {
+      interactiveShellInit = ''
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        fi
+      '';
+    };
   };
 }
